e25383bd6c815484ae5e657cb5fcd19717f3b9b0
Afwan-Pratama <phil.pratama@gmail.com>
2023-03-10 05:05:47 +0700

    add : get all product procedure on trpc

--- src/trpc/router/products.ts
+++ src/trpc/router/products.ts
@@ -4,4 +4,17 @@ export const productRouter = trpcRouter({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.db.product.findMany();
   }),
+  getAllWithRelation: publicProcedure.query(({ ctx }) => {
+    return ctx.db.product.findMany({
+      include: {
+        supplier: {
+          select: {
+            name: true,
+            description: true,
+            address: true,
+          },
+        },
+      },
+    });
+  }),
 });
